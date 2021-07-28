90baecb2960c7fc3105344409a638c650506d7f3
sweatha rao <sweathar@xilinx.com>
2021-07-27 17:04:10 -0700

    isp_quad: Updated 2021.1 isppipeline from vision/L1 for quad sensor

---
+++ platforms/vivado/ip/isp_quad/hdl/verilog/ISPPipeline_accel_demosaicing_3_1_10_2160_3840_2_false_s_linebuffer_V_0.v
@@ -0,0 +1,47 @@
+// ==============================================================
+// Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2021.1 (64-bit)
+// Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+// ==============================================================
+`timescale 1 ns / 1 ps
+module ISPPipeline_accel_demosaicing_3_1_10_2160_3840_2_false_s_linebuffer_V_0 (address0, ce0, d0, we0, q0, address1, ce1, q1,  reset,clk);
+
+parameter DataWidth = 32;
+parameter AddressWidth = 11;
+parameter AddressRange = 1920;
+
+input[AddressWidth-1:0] address0;
+input ce0;
+input[DataWidth-1:0] d0;
+input we0;
+output reg[DataWidth-1:0] q0;
+input[AddressWidth-1:0] address1;
+input ce1;
+output reg[DataWidth-1:0] q1;
+input reset;
+input clk;
+
+(* ram_style = "block" *)reg [DataWidth-1:0] ram[0:AddressRange-1];
+
+
+
+
+always @(posedge clk)  
+begin 
+    if (ce0) begin
+        if (we0) 
+            ram[address0] <= d0; 
+        q0 <= ram[address0];
+    end
+end
+
+
+always @(posedge clk)  
+begin 
+    if (ce1) begin
+        q1 <= ram[address1];
+    end
+end
+
+
+endmodule
+
---
+++ platforms/vivado/ip/isp_quad/hdl/vhdl/ISPPipeline_accel_xf_QuatizationDithering_10_9_2160_3840_256_65536_2_s_offset_buffer_V_0_1.vhd
@@ -0,0 +1,75 @@
+-- ==============================================================
+-- Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2021.1 (64-bit)
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- ==============================================================
+--
+library ieee; 
+use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all;
+
+entity ISPPipeline_accel_xf_QuatizationDithering_10_9_2160_3840_256_65536_2_s_offset_buffer_V_0_1 is 
+    generic(
+            DataWidth     : integer := 13; 
+            AddressWidth     : integer := 11; 
+            AddressRange    : integer := 1920
+    ); 
+    port (
+          address0     : in std_logic_vector(AddressWidth-1 downto 0); 
+          ce0       : in std_logic; 
+          d0        : in std_logic_vector(DataWidth-1 downto 0); 
+          we0       : in std_logic; 
+          address1     : in std_logic_vector(AddressWidth-1 downto 0); 
+          ce1       : in std_logic; 
+          q1        : out std_logic_vector(DataWidth-1 downto 0);
+          reset      : in std_logic; 
+          clk        : in std_logic 
+    ); 
+end entity; 
+
+
+architecture rtl of ISPPipeline_accel_xf_QuatizationDithering_10_9_2160_3840_256_65536_2_s_offset_buffer_V_0_1 is 
+
+signal address1_tmp : std_logic_vector(AddressWidth-1 downto 0); 
+type mem_array is array (0 to AddressRange-1) of std_logic_vector (DataWidth-1 downto 0); 
+signal ram : mem_array;
+
+
+begin 
+
+
+
+p_memory_access_0: process (clk)  
+begin 
+    if (clk'event and clk = '1') then
+        if (ce0 = '1') then 
+            if (we0 = '1') then 
+                ram(CONV_INTEGER(address0)) <= d0; 
+            end if;
+        end if;
+    end if;
+end process;
+
+memory_access_guard_1: process (address1) 
+begin
+      address1_tmp <= address1;
+--synthesis translate_off
+      if (CONV_INTEGER(address1) > AddressRange-1) then
+           address1_tmp <= (others => '0');
+      else 
+           address1_tmp <= address1;
+      end if;
+--synthesis translate_on
+end process;
+
+p_memory_access_1: process (clk)  
+begin 
+    if (clk'event and clk = '1') then
+        if (ce1 = '1') then 
+            q1 <= ram(CONV_INTEGER(address1_tmp));
+        end if;
+    end if;
+end process;
+
+
+end rtl;
+
