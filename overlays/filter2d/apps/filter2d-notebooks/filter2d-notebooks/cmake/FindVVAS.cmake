8d6f1ebe9ce5b99cde943bb1abc53a3af404a5a9
Christian Kohn <christian.kohn@xilinx.com>
2022-06-09 01:17:35 +0200

    filter2d-notebooks: Update vvas cmake module file
    Use PkgConfig in the VVAS cmake module file since there is as module spec file
    for vvas-utils. Also specify the minimum version to be 2.0 as there are code
    changes required due to API changes in 2.0 that are not backward compatible.

--- overlays/filter2d/apps/filter2d-notebooks/filter2d-notebooks/cmake/FindVVAS.cmake
+++ overlays/filter2d/apps/filter2d-notebooks/filter2d-notebooks/cmake/FindVVAS.cmake
@@ -15,12 +15,5 @@
 # VVAS_LIBRARIES - the VVAS libraries
 #
 
-find_path(VVAS_INCLUDE_DIRS NAMES vvas/vvas_kernel.h)
-find_library(VVAS_LIBRARIES NAMES vvasutil)
-
-set (_VVAS_REQUIRED_VARS VVAS_LIBRARIES VVAS_INCLUDE_DIRS)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(VVAS REQUIRED_VARS ${_VVAS_REQUIRED_VARS})
-
-mark_as_advanced(VVAS_INCLUDE_DIRS VVAS_LIBRARIES)
+find_package(PkgConfig)
+pkg_check_modules(VVAS REQUIRED vvas-utils>=2.0)
