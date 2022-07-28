b1766b11f6cac6cbad393f469ad0e180e0efa700
vivekana <vivekana@xilinx.com>
2022-07-28 14:20:03 -0700

    recipes-apps : Generate graphviz config file
    There seems to be a rece condition with the  post install scripts for
    graphviz, which fails to execute dot -c . this generates a config file
    required to convert the dot graph to an image format.

--- petalinux/xilinx-vck190-base-trd/project-spec/meta-base-trd/recipes-apps/trd-files/files/trd-autostart.sh
+++ petalinux/xilinx-vck190-base-trd/project-spec/meta-base-trd/recipes-apps/trd-files/files/trd-autostart.sh
@@ -13,3 +13,6 @@ echo "0" > /proc/sys/kernel/printk
 # update xclbin filename for VART
 echo "Updating xclbin fileanme for /etc/vart.conf ..."
 sed -i 's|/run/media/mmcblk0p1/dpu.xclbin|/boot/binary_container_1.xclbin|g' /etc/vart.conf
+
+# Generate graphviz config file
+/usr/bin/dot -c
